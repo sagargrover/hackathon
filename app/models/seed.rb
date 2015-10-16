448c85b0a5a1d27ed49857cefacd1f6493c188d0
Harman Singh <harman28@gmail.com>
2015-10-16 17:53:07 +0530

    More fields for nearby response

--- app/models/seed.rb
+++ app/models/seed.rb
@@ -1,3 +1,15 @@
 class Seed < ActiveRecord::Base
   validates :name, :creator_id, :coordinates, :presence => true
+
+  def taglist
+  	Tag.where(seed_id:self.id).pluck(:tagged_user_id)
+  end
+
+  def taghandles
+  	Tag.joins("inner join users on tagged_user_id=user_id").where(seed_id:self.id).pluck(:handle)
+  end
+
+  def creator_handle
+  	User.find_by(user_id:self.creator_id).handle
+  end
 end

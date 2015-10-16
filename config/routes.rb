1d512f1c5eed19cde20eddbf942645bf17674a56
Harman Singh <harman28@gmail.com>
2015-10-16 22:18:57 +0530

    Saw API

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,15 @@ Mash::Application.routes.draw do
   root 'welcome#index'
   resources :seeds
   
-    post 'user/create_user' => 'user#new_user'
-    get 'user/get_handle' => 'user#get_handle'
-    post 'api/v1/seeds/nearby' => 'seeds#nearby'
-    get 'api/v1/user/suggest' => 'user#suggest'
-    get 'user/seeds/:user' => 'user#myseeds'
-    get 'user/plants/:user' => 'user#myplants'
-    post 'tag/register_video' => 'tag#register_video'
+  post 'tag/register_video' => 'tag#register_video'
+
+  post 'user/create_user' => 'user#new_user'
+  get 'user/seeds/:user_id' => 'user#myseeds'
+  get 'user/plants/:user_id' => 'user#myplants'
+  get 'user/get_handle' => 'user#get_handle'
+
+  put 'user/:user_id/saw/:seed_id' => 'seen#create'
+
+  post 'api/v1/seeds/nearby' => 'seeds#nearby'
+  get 'api/v1/user/suggest' => 'user#suggest'
 end

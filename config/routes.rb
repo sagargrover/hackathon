2b893b4b16ceed9cbf553a640560af600e75a57e
sagargrover <sagargrover16@gmail.com>
2015-10-16 14:06:51 +0530

    Signup completed

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Mash::Application.routes.draw do
   # root 'welcome#index'
 
   # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+     post 'user/create_user' => 'user#new_user'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151016070751) do
+ActiveRecord::Schema.define(version: 20151016073255) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: true do |t|
     t.string "handle"
-    t.string "password"
+    t.text   "auth_token"
+    t.string "name"
+    t.text   "user_id"
   end
 
 end

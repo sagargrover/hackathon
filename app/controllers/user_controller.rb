b7890e68cecce8d1c47124bf01dfcb5e88d4fcfc
Harman Singh <harman28@gmail.com>
2015-10-16 19:01:11 +0530

    Merge things

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,12 +1,13 @@
 class UserController < ApplicationController
+  include SearchHelper
 	skip_before_filter :verify_authenticity_token 
 	def new_user
 		flag = User.create params[:user_id], params[:fb_auth_token], params[:name]
 		if flag == 1
-	      render :json => {:messages => "Saved"}, :status => :ok
-	    else
-	      render :json => {:messages => "Not Saved"}, :status => 422
-	    end
+      render :json => {:messages => "Saved"}, :status => :ok
+	  else
+	    render :json => {:messages => "Not Saved"}, :status => 422
+	  end
 	end
 
 	def get_handle
